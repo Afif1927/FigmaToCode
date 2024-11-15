b60733e7eb7893a6c1f7d1419ad0b5296e220f15
Mohammad Afif <afifarfan808@gmail.com>
2024-11-15 11:06:20 +0600

    newly updated

--- src/components/Frame36.jsx
+++ src/components/Frame36.jsx
@@ -1,9 +1,9 @@
 import Nextcent from "../assets/NextCent logo.png";
 const Frame36 = () => {
   return (
-    <div className='flex justify-center py-5 md:py-0  md:h-[84px] frame36  items-center gap-8 md:gap-6 lg:gap-[167px]   '>
+    <div className='flex justify-center py-5 md:py-0  md:h-[84px] frame36  items-center gap-10 md:gap-6 lg:gap-[167px]   '>
       <img
-        className='h-[4vw] md:max-w-[154px] md:max-h-[24px] '
+        className='h-[6vw] md:max-w-[154px] md:max-h-[24px] '
         src={Nextcent}
         alt='Logo'
       />
@@ -15,10 +15,10 @@ const Frame36 = () => {
         <a href='#'>FAQ</a>
       </div>
       <div className='login rounded flex  gap-2 lg:gap-[14px]'>
-        <button className='btn1 text-[2.5vw] md:text-[14px] px-2 py-1 sm:px-5 sm:py-2 md:px-[20px] md:py-[10px] font-medium   bg-[#F5F7FA] rounded  text-[#4CAF4F]'>
+        <button className='btn1 text-[3.5vw] md:text-[14px] px-3 py-2 sm:px-5 sm:py-2 md:px-[20px] md:py-[10px] font-medium   bg-[#F5F7FA] rounded  text-[#4CAF4F]'>
           Login
         </button>
-        <button className='btn1 text-[2.5vw] md:text-[14px] px-2 py-1 sm:px-5 sm:py-2 md:px-[20px] md:py-[10px] font-medium text-center text-[#F5F7FA] rounded bg-[#4CAF4F]'>
+        <button className='btn1 text-[3.5vw] md:text-[14px] px-3 py-2 sm:px-5 sm:py-2 md:px-[20px] md:py-[10px] font-medium text-center text-[#F5F7FA] rounded bg-[#4CAF4F]'>
           Sign Up
         </button>
       </div>
